e0fc78fe1ad840a16ecc2c4f48ddcfe0cc16bd4d
unknown <jlarson129@outlook.com>
2021-12-12 10:29:40 -0500

    actions middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,44 @@
-// add middlewares here related to actions
+const Actions = require('./actions-model');
+const Projects = require('../projects/projects-model')
+
+
+const checkID = (req, res, next) => {
+    const { id } = req.params;
+    Actions.get(id)
+        .then(action => {
+            if(!action || action === null) {
+                res.status(404).json({message: "that action does not exist"})
+            } else {
+                req.action = action;
+                next()
+            }
+        })
+}
+
+const checkProjectID = (req,res,next) => {
+    const projectID = req.body.project_id
+    Projects.get(projectID)
+        .then(project => {
+            if(!project) {
+                res.status(404).json({message: "project ID does not exist"})
+            } else {
+                next()
+            }
+
+        })
+}
+
+const checkBody = (req,res,next) => {
+    const newAction = req.body;
+    if(!newAction.project_id || !newAction.description || !newAction.notes) {
+        res.status(400).json({message: "new actions must include project_id, description and notes"})
+    } else {
+        next()
+    }
+}
+
+module.exports = {
+    checkID,
+    checkProjectID,
+    checkBody,
+}
--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,30 @@
-// add middlewares here related to projects
+const Project = require("./projects-model")
+
+
+const checkID = (req,res,next) => {
+    const { id } = req.params;
+    Project.get(id)
+        .then(project => {
+            if (!project) {
+                res.status(404).json({message: "that project does not exist"})
+            } else {
+                next()
+            }
+        })   
+}
+
+const checkBody = (req,res,next) => {
+    const newProject = req.body;
+    if (!newProject.name || !newProject.description) {
+        res.status(400).json({message: "new projects require name and description"})
+    } else {
+        next()
+    }
+    
+}
+
+
+module.exports = {
+    checkID,
+    checkBody,
+}
