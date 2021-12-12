4ce3cf61843fb24c831826ea0427a6988c1f43fb
unknown <jlarson129@outlook.com>
2021-12-12 09:56:08 -0500

    projects router get all and get by id

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,26 @@
-// Write your "projects" router here!
+const Projects = require('./projects-model');
+const express = require("express")
+const router = express.Router()
+const mw = require('./projects-middleware')
+
+router.get("/", (req,res) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({message: "There was an error retrieving the projects"})
+        })
+})
+
+router.get("/:id",mw.checkID, (req,res) => {
+    Projects.get(req.params.id)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({message: "There was an error retrieving the projects"})
+        })
+})
